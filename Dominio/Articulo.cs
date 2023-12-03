cc3c03e73c0d7df0a8b4b271a6fe577d79ebde62
paulivingston <pau.livingston@gmail.com>
2023-12-03 18:17:41 -0300

    clase articulo

---
+++ Dominio/Articulo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Articulo
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public Marca Marca { get; set; }
+        public Categoria Categoria { get; set; }
+        public decimal Precio { get; set; }
+        public bool Estado { get; set; } 
+        public int Stock { get; set; }
+        public string ImagenUrl { get; set; }
+
+        public Articulo()
+        {
+            Marca = new Marca();
+            Categoria = new Categoria();
+        }
+        public Articulo(int id, string nombre, string descripcion, Marca marca, Categoria categoria, decimal precio, bool estado, int stock)
+        {
+            Id = id;
+            Nombre = nombre;
+            Descripcion = descripcion;
+            Marca = marca;
+            Categoria = categoria;
+            Precio = precio;
+            Estado = estado;
+            Stock = stock;
+        }
+    }
+}
