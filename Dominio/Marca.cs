26d388c92bb68034bd9e0c1d867bd5c3d343515c
paulivingston <pau.livingston@gmail.com>
2023-12-03 18:17:53 -0300

    clases categoria y marca

---
+++ Dominio/Marca.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Marca
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+        public string ImagenUrl { get; set; }
+
+        public Marca() {}
+        public Marca(int id, string descripcion, string img)
+        {
+            Id = id;
+            Descripcion = descripcion;
+            ImagenUrl = img;
+        }
+    }
+}
