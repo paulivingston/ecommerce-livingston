c2e6f522af2786aa8190727adb86c53cafee2ec1
paulivingston <pau.livingston@gmail.com>
2023-12-05 02:09:02 -0300

    filtros

---
+++ ecommerce-livingston/Productos.aspx
@@ -0,0 +1,60 @@
+﻿<%@ Page Title="Productos" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Productos.aspx.cs" Inherits="ecommerce_livingston.Productos" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="container mt-5 pt-5 min-vh-100" >
+        <h4 class="mb-5 text-center fs-3 text-bg-dark">Catalogo</h4>
+        <div class="row g-0">
+
+            <div class="col-md-3 bg-light border mb-3 p-1">
+                <div class="p-5">
+                    <asp:DropDownList id="ddlFiltroCategoria" CssClass="form-select h-1 mb-4" runat="server" AutoPostBack="false"></asp:DropDownList>
+                    <asp:DropDownList id="ddlFiltroMarca" CssClass="form-select h-1 mb-4" runat="server" AutoPostBack="false"></asp:DropDownList>
+                    <asp:Button CssClass="btn btn-primary btn-sm mb-5" Text="Aplicar" runat="server" id="btnFiltro" OnClick="btnFiltro_Click" />
+                    <asp:Button CssClass="btn btn-info btn-sm mb-5" Text="Eliminar Filtros" runat="server" id="btnEliminarFilros" OnClick="btnEliminarFilros_Click" />
+                </div>
+            </div>
+
+            <div class="col-md-1"></div>
+            <div class="col-md-8 ">
+                <asp:UpdatePanel runat="server">
+                    <ContentTemplate>
+                        <asp:Repeater ID="repArticulos" runat="server">
+                            <ItemTemplate>
+                                <div class="card mb-3 ms-4" style="max-width: 740px;">
+                                    <div class="row g-0 m-4">
+                                        <div class="col-md-4">
+                                            <img src="<%#Eval("ImagenUrl") %>" class="img-fluid rounded-start" alt="...">
+                                        </div>
+                                        <div class="col-md-8">
+                                            <div class="card-body ">
+                                                <h5 class="card-title fs-3"><%#Eval("Nombre") %></h5>
+                                                <p class="card-text text-primary fs-4 mt-4">$ <%#Eval("Precio")%></p>
+                                                <asp:Button Text="🛒  Agregar" type="button" runat="server" ID="btnAgregar" CssClass="btn btn-warning mt-4" CommandArgument='<%# Eval("Id")%>' OnClick="btnAgregar_Click" />
+                                                <asp:Button Text="ℹ Detalles" type="button" runat="server" ID="btnDetalles" CssClass="btn btn-primary mt-4" CommandArgument='<%# Eval("Id")%>' OnClick="btnDetalles_Click" />
+                                               
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </ItemTemplate>
+                        </asp:Repeater>
+                    </ContentTemplate>
+                </asp:UpdatePanel>
+            </div>
+        </div>
+    </div>
+    <div class="container text-center bg-white border rounded-3 p-3">
+        <div class="row aling-items-center">
+            <div class="col">
+                <div>
+                    <p><a href='<%:Request.UrlReferrer != null? Request.UrlReferrer.ToString() : "/" %>' class="link-dark">Volver</a></p>
+                </div>
+                <div>
+                    <p><a href="Default.aspx" class="link-dark">Volver al inicio</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</asp:Content>
---
+++ ecommerce-livingston/Productos.aspx.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Dominio;
+using Negocio;
+
+namespace ecommerce_livingston
+{
+    public partial class Productos : System.Web.UI.Page
+    {
+        NegocioCategoria NegocioCategoria;
+        NegocioMarca NegocioMarca;
+        NegocioArticulo negocioArticulos = null;
+        Articulo articulo;
+        ItemCarrito item;
+        public int CountCarrito { get; set; } = 0;
+        public string Filtro { get; set; } = string.Empty;
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if (IsPostBack == false)
+                {
+                    List<Articulo> listaArticulos;
+                    cargarFiltros();
+
+                    if (Session["listaCarrito"] == null)
+                        Session.Add("listaCarrito", new NegocioItemCarrito());
+
+                    if (Session["listaPrincipal"] == null)
+                        Session.Add("listaPrincipal", new NegocioArticulo().ListarArticulos());
+
+                    if (Session["listaFiltrada"] == null)
+                        Session.Add("listaFiltrada", new List<Articulo>());
+
+                    if (Request.Params["idCategoria"] != null)
+                    {
+                        listaArticulos = filtrarLista(int.Parse(Request.Params["idCategoria"]), "idCategoria");
+                        Filtro = listaArticulos[0]?.Categoria.Descripcion;
+                    }
+                    else if (Request.Params["idMarca"] != null)
+                    {
+                        listaArticulos = filtrarLista(int.Parse(Request.Params["idMarca"]), "idMarca");
+                        Filtro = listaArticulos[0]?.Marca.Descripcion;
+                    }
+                    else if (Request.Params["text"] != null)
+                    {
+                        listaArticulos = filtrarLista(Request.Params["text"]);
+                        Filtro = Request.Params["text"].ToString();
+                    }
+                    else
+                    {
+                        listaArticulos = (List<Articulo>)Session["ListaPrincipal"];
+                        Filtro = "";
+                    }
+                    repArticulos.DataSource = listaArticulos;
+                    repArticulos.DataBind();
+                }
+            }
+            catch (Exception ex)
+            {
+                Session.Add("error", ex);
+                Response.Redirect("Error.aspx");
+            }
+        }
+
+
+        private void cargarFiltros()
+        {
+            NegocioMarca = new NegocioMarca();
+            NegocioCategoria = new NegocioCategoria();
+
+            ddlFiltroMarca.DataSource = NegocioMarca.ListarMarcas();
+            ddlFiltroMarca.DataBind();
+            ddlFiltroMarca.DataValueField = "Id";
+            ddlFiltroMarca.DataTextField = "Descripcion";
+
+            ddlFiltroCategoria.DataSource = NegocioCategoria.ListarCategorias();
+            ddlFiltroCategoria.DataBind();
+            ddlFiltroCategoria.DataValueField = "Id";
+            ddlFiltroCategoria.DataTextField = "Descripcion";
+        }
+
+
+        private List<Articulo> filtrarLista(object filtro, string tipoFiltro = null)
+        {
+            try
+            {
+                if (Session["listaPrincipal"] == null)
+                    return null;
+
+                List<Articulo> listaPrincipal = (List<Articulo>)Session["listaPrincipal"];
+                List<Articulo> listaFiltrada = (List<Articulo>)Session["listaFiltrada"];
+
+                if (filtro is string)
+                {
+                    string item = filtro as string;
+                    listaFiltrada = listaPrincipal.FindAll(
+                        x => (x.Nombre.ToUpperInvariant().Contains(item.ToUpperInvariant()) ||
+                        x.Marca.Descripcion.ToUpperInvariant().Contains(item.ToUpperInvariant()))
+                        );
+                }
+                else if (filtro is int)
+                {
+                    int item = (int)filtro;
+
+                    if (tipoFiltro == "idCategoria")
+                        listaFiltrada = listaPrincipal.FindAll(art => art.Categoria.Id == item);
+                    else if (tipoFiltro == "idMarca")
+                        listaFiltrada = listaPrincipal.FindAll(art => art.Marca.Id == item);
+                }
+                return listaFiltrada;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void totalItemsCarrito()
+        {
+            if (Session["cantidadItems"] == null)
+                Session.Add("cantidadItems", 0);
+
+            int total = (int)Session["cantidadItems"];
+            total++;
+            Session["cantidadItems"] = total;
+
+            Label lblcantidad = (Label)Master.FindControl("lblTotalArticulos");
+            if (lblcantidad != null)
+            {
+                lblcantidad.Text = total.ToString();
+            }
+        }
+
+        protected void btnFiltro_Click(object sender, EventArgs e)
+        {
+            List<Articulo> listaPrincipal = (List<Articulo>)Session["listaPrincipal"];
+            try
+            {
+                repArticulos.DataSource = listaPrincipal.FindAll(x =>
+                    x.Categoria.Descripcion.Contains(ddlFiltroCategoria.Text)
+                    && x.Marca.Descripcion.Contains(ddlFiltroMarca.Text));
+                repArticulos.DataBind();
+            }
+            catch (Exception ex)
+            {
+                Session.Add("error", ex);
+                Response.Redirect("Error.aspx");
+            }
+        }
+
+        protected void btnEliminarFilros_Click(object sender, EventArgs e)
+        {
+            Session.Remove("listaFiltrada");
+            Response.Redirect("Productos.aspx", false);
+        }
+
+        protected void btnDetalles_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(((Button)sender).CommandArgument);
+
+            Response.Redirect("Detalle.aspx?idProd=" + id, false);
+        }
+
+        protected void btnAgregar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int itemId = int.Parse((sender as Button).CommandArgument);
+                negocioArticulos = new NegocioArticulo();
+                item = new ItemCarrito();
+
+                List<Articulo> list = Session["listaPrincipal"] as List<Articulo>;
+                NegocioItemCarrito carrito = Session["listaCarrito"] as NegocioItemCarrito;
+
+                articulo = list.Find(art => art.Id == itemId); 
+                if (articulo != null)
+                {
+                    item.Id = itemId;
+                    item.Nombre = articulo.Nombre;
+                    item.Precio = articulo.Precio;
+                    item.ImagenUrl = articulo.ImagenUrl;
+                    item.Cantidad = 1;
+                    carrito.AgregarItem(item);
+                }
+
+                totalItemsCarrito();
+            }
+            catch (Exception ex)
+            {
+                Session.Add("error", ex);
+                Response.Redirect("Error.aspx");
+            }
+        }
+
+    }
+}
