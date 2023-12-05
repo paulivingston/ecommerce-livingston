fccacc1ab8c47b762bed7af461e4e4d5657c9333
paulivingston <pau.livingston@gmail.com>
2023-12-05 02:06:30 -0300

    clase para items del carrito

---
+++ Dominio/ItemCarrito.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class ItemCarrito
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public int Cantidad { get; set; }
+        public string ImagenUrl { get; set; }
+        public int IdPedido { get; set; } 
+        public string Descripcion { get; set; }
+        public string Marca { get; set; }
+        public string Categoria { get; set; }
+        public string Estado { get; set; }
+        public int Stock { get; set; }
+    }
+}
