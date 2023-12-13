524fd44a327ab0b2de565e6a86eb98ae2e3f8e3d
paulivingston <pau.livingston@gmail.com>
2023-12-13 17:29:50 -0300

    detalle producto

--- ecommerce-livingston/ECOMMERCELIVINGSTON.sql
+++ ecommerce-livingston/ECOMMERCELIVINGSTON.sql
@@ -420,6 +420,9 @@ AS
 BEGIN
 	DELETE FROM PEDIDOS 
 	WHERE IdPedido = @id
+
+	DELETE FROM PEDIDO_ARTICULO 
+	WHERE IdPedido = @Id
 END
 GO
 
@@ -434,7 +437,295 @@ BEGIN
 END
 GO
 
+CREATE PROCEDURE sp_ListarImgArt
+@id int
+AS
+BEGIN
+	SELECT ImagenUrl 
+	FROM IMAGENES 
+	WHERE IdArticulo = @id
+END
+GO
+
+CREATE PROCEDURE sp_ListarUsuarios
+AS
+BEGIN
+	SELECT 
+		Id, 
+		Nombre, 
+		Apellido, 
+		DNI, 
+		Mail, 
+		Clave, 
+		Direccion, 
+		Nivel, 
+		ImagenUrl, 
+		Activo 
+	FROM USUARIOS
+END
+GO
+
+CREATE PROCEDURE sp_ListarUsuariosPorID
+@id int
+AS
+BEGIN
+	SELECT 
+		Id, 
+		Nombre, 
+		Apellido, 
+		DNI, 
+		Mail, 
+		Clave, 
+		Direccion, 
+		Nivel, 
+		ImagenUrl, 
+		Activo 
+	FROM USUARIOS
+	WHERE Id = @id
+END
+GO
+
+CREATE PROCEDURE sp_ListarUsuariosLogin
+@Mail varchar(30)
+AS
+BEGIN
+	SELECT 
+		Id, 
+		Nombre, 
+		Apellido, 
+		DNI, 
+		Mail, 
+		Clave, 
+		Direccion, 
+		Nivel, 
+		ImagenUrl, 
+		Activo 
+	FROM USUARIOS
+	WHERE Mail = @Mail 
+END
+GO
+
+CREATE PROCEDURE sp_CrearUsuario
+@Nombre varchar(30), 
+@Apellido varchar(30), 
+@DNI int, 
+@Mail varchar(30), 
+@Clave varchar(30), 
+@Direccion varchar(100), 
+@Nivel char(1), 
+@UrlImagen  varchar(max), 
+@Activo bit
+AS
+BEGIN
+	INSERT INTO USUARIOS (
+		Nombre, 
+		Apellido, 
+		DNI, 
+		Mail, 
+		Clave, 
+		Direccion, 
+		Nivel, 
+		ImagenUrl, 
+		Activo
+	) 
+	VALUES (
+		@Nombre, 
+		@Apellido, 
+		@DNI, 
+		@Mail, 
+		@Clave, 
+		@Direccion, 
+		@Nivel, 
+		@UrlImagen, 
+		@Activo
+	)
+END
+GO
+
+CREATE PROCEDURE sp_ModificarUsuario
+@Id int,
+@Nombre varchar(30), 
+@Apellido varchar(30), 
+@DNI int, 
+@Mail varchar(30), 
+@Clave varchar(30), 
+@Direccion varchar(100), 
+@Nivel char(1), 
+@UrlImagen  varchar(max), 
+@Activo bit
+AS
+BEGIN
+	UPDATE USUARIOS 
+	SET 
+		Nombre = @Nombre, 
+		Apellido = @Apellido, 
+		DNI = @DNI, 
+		Mail = @Mail, 
+		Clave = @Clave, 
+		Direccion = @Direccion, 
+		Nivel = @Nivel, 
+		ImagenUrl = @UrlImagen, 
+		Activo = @Activo 
+	WHERE Id = @Id
+END
+GO
+
+CREATE PROCEDURE sp_EliminarUsuario
+@Id int
+AS
+BEGIN
+	DELETE FROM USUARIOS 
+	WHERE Id = @id
+END
+GO
+
+CREATE PROCEDURE sp_AltaUsuario
+@Id int
+AS
+BEGIN
+	UPDATE USUARIOS 
+	SET Activo = 1 
+	WHERE Id = @id
+END
+GO
+
+CREATE PROCEDURE sp_BajaUsuario
+@Id int
+AS
+BEGIN
+	UPDATE USUARIOS 
+	SET Activo = 0 
+	WHERE Id = @id
+END
+GO
+
+CREATE PROCEDURE sp_ComprobarIdUsuario
+@id int
+AS
+BEGIN
+	SELECT COUNT(*) 
+	FROM USUARIOS 
+	WHERE Id = @id
+END
+GO
+
+CREATE PROCEDURE sp_ListarArticulosPedido
+AS
+BEGIN
+	SELECT 
+		IdPedido, 
+		IdArticulo, 
+		Cantidad 
+	FROM PEDIDO_ARTICULO
+END
+GO
+
+CREATE PROCEDURE sp_ListarArticulosPedidoPorID
+@id int
+AS
+BEGIN
+	SELECT 
+		IdPedido, 
+		IdArticulo, 
+		Cantidad 
+	FROM PEDIDO_ARTICULO
+	WHERE IdPedido = @id
+END
+GO
+
+CREATE PROCEDURE sp_AgregarArticuloPedido
+@IdPedido int,
+@IDArticulo int,
+@Cantidad int
+AS
+BEGIN
+	INSERT INTO PEDIDO_ARTICULO 
+	VALUES (
+		@IdPedido, 
+		@IdArticulo, 
+		@Cantidad
+	)
+END
+GO
+
+CREATE PROCEDURE sp_EditarArticuloPedido
+@IdPedido int,
+@IDArticulo int,
+@Cantidad int
+AS
+BEGIN
+	UPDATE PEDIDO_ARTICULO 
+	SET 
+		IdArticulo = @IdArticulo, 
+		Cantidad = @Cantidad 
+	WHERE IdPedido = @IdPedido
+END
+GO
+
+CREATE PROCEDURE sp_EliminarArticuloPedido
+@IdPedido int,
+@IdArticulo int
+AS
+BEGIN
+	DELETE FROM PEDIDO_ARTICULO 
+	WHERE IdPedido = @IdPedido
+	AND IdArticulo = @IdArticulo
+END
+GO
 
 
 
 ------------FIN STORE PROCEDURES------------
+
+
+
+------------DUMMY DATA------------
+
+
+
+INSERT INTO [dbo].[CATEGORIAS] ([Id],[Descripcion],[ImagenUrl]) VALUES (0,'SUN','https://www.masvision.com.ar/cdn/shop/files/VULKGENIEMBLKGREENEMERALD_800x_5a25ed4d-aacd-488e-b6b0-c432016884b6_800x503.jpg?v=1699153586')
+INSERT INTO [dbo].[CATEGORIAS] ([Id],[Descripcion],[ImagenUrl]) VALUES (1,'OPTICS','https://www.masvision.com.ar/cdn/shop/products/PCDA_Hazel_col01_RX_Frente_870x574.jpg?v=1674487566')
+INSERT INTO [dbo].[CATEGORIAS] ([Id],[Descripcion],[ImagenUrl]) VALUES (2,'CONTACTS','https://www.masvision.com.ar/cdn/shop/products/Air-Optix-Plus-Hydraglyde-multifocal-550x440_550x440.jpg?v=1701444968')
+INSERT INTO [dbo].[CATEGORIAS] ([Id],[Descripcion],[ImagenUrl]) VALUES (3,'GOGGLES','https://acdn.mitiendanube.com/stores/600/169/products/ziro-c2-vista-2_final1-7bc2b67baf1b98968d16527473812183-480-0.webp')
+
+INSERT INTO [dbo].[MARCAS] ([Id],[Descripcion],[ImagenUrl]) VALUES (0,'Ray-Ban','https://www.masvision.com.ar/cdn/shop/files/marca_Ray-Ban_small.png?v=1697029603')
+INSERT INTO [dbo].[MARCAS] ([Id],[Descripcion],[ImagenUrl]) VALUES (1,'Vulk','https://www.masvision.com.ar/cdn/shop/files/marca_Vulk_small.png?v=1697029603')
+INSERT INTO [dbo].[MARCAS] ([Id],[Descripcion],[ImagenUrl]) VALUES (2,'Vogue','https://www.masvision.com.ar/cdn/shop/files/marca_Vogue_small.png?v=1697029603')
+INSERT INTO [dbo].[MARCAS] ([Id],[Descripcion],[ImagenUrl]) VALUES (3,'Oakley','https://www.masvision.com.ar/cdn/shop/files/marca_Oakley_small.png?v=1697029603')
+INSERT INTO [dbo].[MARCAS] ([Id],[Descripcion],[ImagenUrl]) VALUES (4,'Rusty','https://www.masvision.com.ar/cdn/shop/files/marca_Rusty_small.png?v=1697029603')
+
+INSERT INTO [dbo].[USUARIOS] ([Nombre],[Apellido],[DNI],[Mail],[Clave],[Direccion],[Nivel],[ImagenUrl],[Activo]) VALUES ('Pepe','Pepa',11111111,'pepe.pepa@gmail.com','password1','donde vive pepe 123','N','https://dthezntil550i.cloudfront.net/f4/latest/f41908291942413280009640715/1280_960/1b2d9510-d66d-43a2-971a-cfcbb600e7fe.png',1)
+INSERT INTO [dbo].[USUARIOS] ([Nombre],[Apellido],[DNI],[Mail],[Clave],[Direccion],[Nivel],[ImagenUrl],[Activo]) VALUES ('Pepito','Admin',11111111,'pepito.admin@gmail.com','password2','donde vive pepito 456','A','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwD5S3sw60LDdyNbWwv0Od9IBWQ1vYaAMLEmwizhyb-8HkWJsB5tU2F7ljmaRyfzArkq4&usqp=CAU',1)
+
+INSERT INTO [dbo].[ARTICULOS] ([Id],[Nombre],[Descripcion],[IdMarca],[IdCategoria],[ImagenUrl],[Precio],[Estado],[Stock]) 
+VALUES (0,'Anteojos de Sol Rusty Bruk Sblk Polarizado S10','Género: Unisex
+Color del Armazón: Negro brillante
+Color y Tipo del Lente: negro uniforme polarizado
+Material: Acetato
+Estilo: Cuadrado',4,0,'https://www.masvision.com.ar/cdn/shop/files/BRUKSBLK-POLS10FRENTE_869x576.jpg?v=1683294063',50000,1,500)
+INSERT INTO [dbo].[ARTICULOS] ([Id],[Nombre],[Descripcion],[IdMarca],[IdCategoria],[ImagenUrl],[Precio],[Estado],[Stock]) 
+VALUES (1,'Anteojos de Sol Vulk Arvin Mdblu Revo Blue','Género: Hombre
+Color del Armazón: Azul marino
+Color y Tipo del Lente: Celeste espejado
+Material: Acetato 
+Estilo: Cuadrado',1,0,'https://www.masvision.com.ar/cdn/shop/files/ArvinMDBLUREVOBLUEF_870x580.jpg?v=1683207183',52000,1,500)
+INSERT INTO [dbo].[ARTICULOS] ([Id],[Nombre],[Descripcion],[IdMarca],[IdCategoria],[ImagenUrl],[Precio],[Estado],[Stock]) 
+VALUES (2,'Anteojos de Sol Vulk Boston Mblk Revo Red','Género: Unisex
+Color del Armazón: Negro mate
+Color y Tipo del Lente: Rojo Degrade
+Material: Acetato
+Estilo: Cuadrado',1,0,'https://www.masvision.com.ar/cdn/shop/files/BOSTON-MBLK-REVO-RED---FRENTE--PRINCIPAL-WEB_869x427.jpg?v=1683208233',55000,1,300)
+INSERT INTO [dbo].[ARTICULOS] ([Id],[Nombre],[Descripcion],[IdMarca],[IdCategoria],[ImagenUrl],[Precio],[Estado],[Stock]) 
+VALUES (3,'Anteojos de Sol Rusty Ant Ul C3 Revo Blue','Género: Unisex
+Color del Armazón: Negro
+Color y Tipo del Lente: Azul Espejado
+Material: Metal y Acetato
+Estilo: Redondo',4,0,'https://www.masvision.com.ar/cdn/shop/files/UL---C3---PERFIL_871x428.jpg?v=1683204061',36000,1,5)
+INSERT INTO [dbo].[ARTICULOS] ([Id],[Nombre],[Descripcion],[IdMarca],[IdCategoria],[ImagenUrl],[Precio],[Estado],[Stock]) 
+VALUES (4,'Anteojos de Sol Rusty Marilyn 23 Sblk 940','Género: Unisex
+Color del Armazón: Negro
+Color y Tipo del Lente: verde oscuro
+Material: Acetato
+Estilo: Envolvente ovalado',4,0,'https://www.masvision.com.ar/cdn/shop/products/MARILYN23AGALERIA-WEB-SBLK-940_871x428.jpg?v=1674065465',46000,1,50)
+
+------------FIN DUMMY DATA------------
