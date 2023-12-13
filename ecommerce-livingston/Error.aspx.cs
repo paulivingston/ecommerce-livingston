64a674f65362b4c3bdaaf737bc15b55511852ca2
paulivingston <pau.livingston@gmail.com>
2023-12-13 17:30:08 -0300

    agrego pagina de error

---
+++ ecommerce-livingston/Error.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Negocio;
+using Mensajes;
+
+namespace ecommerce_livingston
+{
+    public partial class Error : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Session["error"] != null)
+                {
+                    Exception ex = (Exception)Session["error"];
+                    string msj = ex.Message;
+                    lblMensaje.Text = "ERROR: \n"+msj;
+                    lblErrorFuente.Text = "FUENTE: " + ex.Source;
+                    lblErrorCompleto.Text = "ERROR COMPLETO: " + ex.ToString();
+                }
+                else if (Session["MensajeError"] != null)
+                {
+                    string msj = Session["MensajeError"].ToString();
+                    lblMensajeError.Text = "Mensaje: \n" + msj;
+                }
+            }
+            catch (Exception ex)
+            {
+                Mensajes.Mensajes.MensajePopUp(this, "Ocurrio un error inesperado\nInforme al equipo de desarrollo");
+                throw ex;
+            }
+        }
+    }
+}
