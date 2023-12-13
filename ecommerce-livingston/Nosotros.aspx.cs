0ef76723d24f0651f1fc3ec195a0db1534d182bd
paulivingston <pau.livingston@gmail.com>
2023-12-13 17:32:05 -0300

    reestructuracion de paginas

--- ecommerce-livingston/Nosotros.aspx.cs
+++ ecommerce-livingston/Nosotros.aspx.cs
@@ -4,14 +4,45 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Dominio;
+using Negocio;
 
 namespace ecommerce_livingston
 {
     public partial class Nosotros : System.Web.UI.Page
     {
+        protected List<string> ImgNos;
+        protected List<Articulo> ArtNos = null;
+        protected Articulo art = null;
+        protected int idMatch = 0;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            try
+            {
+                List<Articulo> articulos = null;
+                if (Session["listaPrincipal"] != null)
+                    articulos = (List<Articulo>)Session["listaPrincipal"];
 
+                if (Request.Params["idProd"] != null && articulos != null)
+                {
+                    NegocioArticulo listaArticulos = new NegocioArticulo();
+                    idMatch = int.Parse(Request.Params["idProd"]);
+                    ImgNos = listaArticulos.ListarImgArt(idMatch);
+                    ArtNos = new List<Articulo>();
+
+                    art = articulos.Find(itm => itm.Id == idMatch);
+                    ArtNos.Add(art);
+
+                    rptDetalleArt.DataSource = ArtNos;
+                    rptDetalleArt.DataBind();
+                }
+            }
+            catch (Exception ex)
+            {
+                Session.Add("error", ex);
+                Response.Redirect("Error.aspx");
+            }
         }
     }
 }
--- ecommerce-livingston/Nosotros.aspx.designer.cs
+++ ecommerce-livingston/Nosotros.aspx.designer.cs
@@ -13,5 +13,14 @@ namespace ecommerce_livingston
 
     public partial class Nosotros
     {
+
+        /// <summary>
+        /// rptDetalleArt control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater rptDetalleArt;
     }
 }
